1dc42425c57fb10641eeaef62c0735d23dd3b01e
UBC Student <reyred@students.cs.ubc.ca>
2023-05-08 15:40:09 -0700

    changed assets, added dynamic display

--- frontend/src/components/game/bouncer/Bouncer.tsx
+++ frontend/src/components/game/bouncer/Bouncer.tsx
@@ -7,6 +7,8 @@ type Props = { xPos: number; yPos: number; number: number; id: string };
 export const Bouncer = ({ xPos, yPos, number }: Props) => {
   const canvasSize = useCanvasSize();
   const displayRatio = canvasSize / 500;
+  const rotation = number * 90;
+
   return (
     <svg
       style={{
@@ -20,24 +22,15 @@ export const Bouncer = ({ xPos, yPos, number }: Props) => {
         width: `${BOUNCER_WIDTH * displayRatio}px`,
         height: `${BOUNCER_HEIGHT * displayRatio}px`,
         transform: `rotate(${number % 2 === 0 ? 90 : 0}deg)`,
+        filter: `hue-rotate(${rotation}deg)`,
       }}
-      viewBox="0 0 451 80"
+      viewBox="0 0 400 80"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect x="0.180542" width="450" height="80" fill="#23D796" />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M10 0H0V5H5V10H10H440V5H445V0H440H10ZM10 25V20V15H60V20H25V25H10ZM70 15H65V20H70V15ZM430 50H435V65H430V50Z"
-        fill="#B3FEE3"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M445.181 0H450.181V80H445.181H440.181H435.181H10.1805H0.180542V75H5.18054V70H10V65H15V70H430.181V65H435.181V10H440.181V5H445.181V0Z"
-        fill="#117954"
-      />
+      <rect width="400" height="80" fill="#23D796" />
+      <path d="M0 0H400L385 15H15L0 0Z" fill="#B3FEE3" />
+      <path d="M385 15L400 0V80H0L15 65H385V15Z" fill="#117954" />
     </svg>
   );
 };
--- frontend/src/components/game/brick/Brick.tsx
+++ frontend/src/components/game/brick/Brick.tsx
@@ -7,6 +7,7 @@ type Props = { xPos: number; yPos: number; id: string; level: number };
 export const Brick = ({ xPos, yPos, level }: Props) => {
   const canvasSize = useCanvasSize();
   const displayRatio = canvasSize / 500;
+  const opacity = (level - 1) * 0.12 + 0.4;
 
   return (
     <svg
@@ -16,6 +17,7 @@ export const Brick = ({ xPos, yPos, level }: Props) => {
         top: `${yPos * displayRatio}px`,
         width: `${BRICK_SIZE * displayRatio}px`,
         height: `${BRICK_SIZE * displayRatio}px`,
+        opacity: opacity,
       }}
       viewBox="0 0 150 150"
       fill="none"
@@ -23,9 +25,15 @@ export const Brick = ({ xPos, yPos, level }: Props) => {
     >
       <rect width="150" height="150" fill="#5A1757" />
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M140 10H105V15H135V40H140V15V10ZM135 45H140V50H135V45ZM10 140H45V135H15V110H10V135V140ZM10 105H15V100H10V105Z"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M0 0H20V20H0V0ZM35 35H115V115H35V35ZM150 130H130V150H150V130Z"
+        fill="#8C4A89"
+      />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M110 10H140V20V50H130V20H110V10ZM20 130V100H10V130V140H40V130H20ZM60 140H50V130H60V140ZM100 10H90V20H100V10Z"
         fill="#FAD2F8"
       />
     </svg>
