78b71cf96df5256c5604158e5fbb85cfa65073b7
rabbitblood <qz154772610@gmail.com>
2023-05-11 10:16:12 -0700

    trying to fix data missing

--- backend/game/BrickMap.ts
+++ backend/game/BrickMap.ts
@@ -11,6 +11,7 @@ export function generateBrickMap(): BrickMap {
   //row
   for (let i = 0; i < GameData.MAX_BRICKS_PER_ROW; i++) {
     map.push([]);
+    //column
     for (let j = 0; j < GameData.MAX_BRICKS_PER_COLUMN; j++) {
       const random = Math.floor(Math.random() * GameData.MAX_BRICK_LIFE);
       map[i].push(new Brick(random));
