9e7cebcc25c6da6f882cf5b8f429e8cb7e2a67e1
rabbitblood <qz154772610@gmail.com>
2023-05-13 21:25:49 -0700

    blockchain route between pages

---
+++ frontend/src/components/BlockChain/NFts.ts
@@ -0,0 +1,6 @@
+import electricNFTContractAbi from "./abi/ElectricBounser.json";
+export const electricNFTContract = {
+  abi: electricNFTContractAbi.abi,
+  address: "0x2A114e5Fc1DF27b80619390DB15E7b41F44e756E",
+  imgUrl: "",
+};
