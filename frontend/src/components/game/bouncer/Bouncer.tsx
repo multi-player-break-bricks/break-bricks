2171059a9b0f000d652d6b77ccdd934dcf36e0c6
Jingxuan <taosit099@gmail.com>
2023-05-15 13:40:26 -0700

    Change skin

--- frontend/src/components/game/bouncer/Bouncer.tsx
+++ frontend/src/components/game/bouncer/Bouncer.tsx
@@ -12,58 +12,29 @@ export const Bouncer = ({ xPos, yPos, height, width, number }: Bouncer) => {
   const displayRatio = canvasSize / 500;
   const rotation = number * 90;
 
-  const normalBouncer: any = () => {
-    return (
-      <svg
-        style={{
-          backgroundColor: "blue",
-          borderRadius: "2px",
-          left: `${
-            xPos * displayRatio +
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          top: `${
-            yPos * displayRatio -
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          width: `${(number % 2 ? width : height) * displayRatio}px`,
-          height: `${(number % 2 ? height : width) * displayRatio}px`,
-          transform: `rotate(${number % 2 === 0 ? 90 : 0}deg)`,
-          filter: `hue-rotate(${rotation}deg)`,
-        }}
-        preserveAspectRatio="none"
-        viewBox="0 0 400 80"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path d="M0 0H400L385 15H15L0 0Z" fill="#B3FEE3" />
-        <path d="M385 15L400 0V80H0L15 65H385V15Z" fill="#117954" />
-      </svg>
-    );
-  };
-
-  const loveBouncer: any = () => {
-    return (
-      <svg
-        style={{
-          backgroundColor: "blue",
-          borderRadius: "2px",
-          left: `${
-            xPos * displayRatio +
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          top: `${
-            yPos * displayRatio -
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          width: `${(number % 2 ? width : height) * displayRatio}px`,
-          height: `${(number % 2 ? height : width) * displayRatio}px`,
-          transform: `rotate(${number % 2 === 0 ? 90 : 0}deg)`,
-          filter: `hue-rotate(${rotation}deg)`,
-        }}
-        viewBox="0 0 400 80"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-      >
+  return (
+    <svg
+      style={{
+        backgroundColor: "blue",
+        borderRadius: "2px",
+        left: `${
+          xPos * displayRatio +
+          ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
+        }px`,
+        top: `${
+          yPos * displayRatio -
+          ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
+        }px`,
+        width: `${(number % 2 ? width : height) * displayRatio}px`,
+        height: `${(number % 2 ? height : width) * displayRatio}px`,
+        transform: `rotate(${number % 2 === 0 ? 90 : 0}deg)`,
+        filter: `hue-rotate(${rotation}deg)`,
+      }}
+      preserveAspectRatio="none"
+      viewBox="0 0 400 80"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {useSkinName === loveNFTContract.skinName && (
         <g>
           <title>Layer 1</title>
           <path
@@ -83,33 +54,8 @@ export const Bouncer = ({ xPos, yPos, height, width, number }: Bouncer) => {
             id="svg_5"
           />
         </g>
-      </svg>
-    );
-  };
-
-  const electricBouncer: any = () => {
-    return (
-      <svg
-        style={{
-          backgroundColor: "blue",
-          borderRadius: "2px",
-          left: `${
-            xPos * displayRatio +
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          top: `${
-            yPos * displayRatio -
-            ((number + 1) % 2) * (((width - height) / 2) * displayRatio)
-          }px`,
-          width: `${(number % 2 ? width : height) * displayRatio}px`,
-          height: `${(number % 2 ? height : width) * displayRatio}px`,
-          transform: `rotate(${number % 2 === 0 ? 90 : 0}deg)`,
-          filter: `hue-rotate(${rotation}deg)`,
-        }}
-        viewBox="0 0 400 80"
-        xmlns="http://www.w3.org/2000/svg"
-        preserveAspectRatio="none"
-      >
+      )}
+      {useSkinName === electricNFTContract.skinName && (
         <g>
           <title>Layer 1</title>
           <path
@@ -195,15 +141,14 @@ export const Bouncer = ({ xPos, yPos, height, width, number }: Bouncer) => {
             fill="#ff0000"
           />
         </g>
-      </svg>
-    );
-  };
-
-  if (useSkinName === loveNFTContract.skinName) {
-    return loveBouncer();
-  } else if (useSkinName === electricNFTContract.skinName) {
-    return electricBouncer();
-  } else {
-    return normalBouncer();
-  }
+      )}
+      {useSkinName !== loveNFTContract.skinName &&
+        useSkinName !== electricNFTContract.skinName && (
+          <>
+            <path d="M0 0H400L385 15H15L0 0Z" fill="#B3FEE3" />
+            <path d="M385 15L400 0V80H0L15 65H385V15Z" fill="#117954" />
+          </>
+        )}
+    </svg>
+  );
 };
